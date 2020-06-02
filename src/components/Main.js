d12be3561d43630fa913f99580a730d9d42e04ce
malan88 <malan88@standingwater.io>
2020-06-02 16:26:45 -0400

    Try to make form work

--- src/components/Main.js
+++ src/components/Main.js
@@ -149,7 +149,7 @@ class Main extends React.Component {
                     style={{ display: 'none' }}
                 >
                     <h2 className="major">Contact</h2>
-                    <form method="post" netlify-honeypot="bot-field" data-netlify="true">
+                    <form name="contact" method="post" data-netlify="true" data-netlify-honeypot="bot-field">
                         <div className="field half first">
                             <label htmlFor="name">Name</label>
                             <input type="text" name="name" id="name" />
