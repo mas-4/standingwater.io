3586f746c403582b45a9dbce96c0fde884d9f298
malan88 <malan88@standingwater.io>
2020-06-02 16:28:16 -0400

    More work on form field

--- src/components/Main.js
+++ src/components/Main.js
@@ -150,6 +150,7 @@ class Main extends React.Component {
                 >
                     <h2 className="major">Contact</h2>
                     <form name="contact" method="post" data-netlify="true" data-netlify-honeypot="bot-field">
+                        <input type="hidden" name="form-name" value="contact" />
                         <div className="field half first">
                             <label htmlFor="name">Name</label>
                             <input type="text" name="name" id="name" />
