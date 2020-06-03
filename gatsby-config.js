fa538437f4f17431d44e99552f488f4eab2149c9
malan88 <malan88@standingwater.io>
2020-06-03 14:23:30 -0400

    gatsby-htaccess

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,24 +1,31 @@
 module.exports = {
-  siteMetadata: {
-    title: 'Michael Sendker',
-    author: 'Michael Sendker',
-    description: 'Michael Sendker is a Python developer specializing in Flask/SQLAlchemy',
-    siteUrl: `https://standingwater.io`
-  },
-  plugins: [
-    'gatsby-plugin-react-helmet',
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: 'gatsby-starter-default',
-        short_name: 'starter',
-        start_url: '/',
-        background_color: '#663399',
-        theme_color: '#663399',
-        display: 'minimal-ui',
-        icon: 'src/images/heron.png', // This path is relative to the root of the site.
-      },
+    siteMetadata: {
+        title: 'Michael Sendker',
+        author: 'Michael Sendker',
+        description: 'Michael Sendker is a Python developer specializing in Flask/SQLAlchemy',
+        siteUrl: `https://standingwater.io`
     },
-    'gatsby-plugin-sass',
-  ],
+    plugins: [
+        'gatsby-plugin-react-helmet',
+        {
+            resolve: 'gatsby-plugin-htaccess',
+            options: {
+                'https': true,
+                'host': 'standingwater.io'
+            }
+        }
+        {
+            resolve: `gatsby-plugin-manifest`,
+            options: {
+                name: 'gatsby-starter-default',
+                short_name: 'starter',
+                start_url: '/',
+                background_color: '#663399',
+                theme_color: '#663399',
+                display: 'minimal-ui',
+                icon: 'src/images/heron.png', // This path is relative to the root of the site.
+            },
+        },
+        'gatsby-plugin-sass',
+    ],
 }
