d56541e8d1f1444b150282d6147fd8e91d14c012
malan88 <m@stdwtr.io>
2020-06-30 16:16:32 -0400

    Fix link for InstaPaper

--- src/components/Header.js
+++ src/components/Header.js
@@ -82,7 +82,7 @@ const Header = props => (
           />
           <SVGIcon
             title="InstaPaper"
-            href="https://www.instapaper.com/p/mas"
+            url="https://www.instapaper.com/p/mas"
             SVG={InstaPaper}
           />
           <Icon
